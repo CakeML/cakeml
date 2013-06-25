CakeML/cakeml

commit 02c127e25edee9e803bd383189114fcdf413d8e7

move bytecodeExtra and compileLabels theories into bytecode dir

compiler/bytecode/Holmakefile
@@ -1,4 +1,4 @@
-INCLUDES = ../../semantics
+INCLUDES = ../../semantics ..
 
 OPTIONS = QUIT_ON_FAILURE
 

compiler/proofs/Holmakefile
@@ -6,7 +6,7 @@ HOLHEAP = heap
 PARENT_HOLHEAP = ../heap
 EXTRA_CLEANS = $(HOLHEAP) $(HOLHEAP).o
 
-BARE_THYS = ../../semantics/terminationTheory ../bytecode/bytecodeTerminationTheory ../compilerTerminationTheory ../../metatheory/bigClockTheory
+BARE_THYS = ../../semantics/terminationTheory ../compilerTerminationTheory ../bytecode/bytecodeExtraTheory ../miscTheory ../../metatheory/bigClockTheory
 DEPS = $(patsubst %,%.uo,$(BARE_THYS))
 
 $(HOLHEAP): $(DEPS)

impl_proofs/Holmakefile
@@ -1,4 +1,4 @@
-INCLUDES=.. ../semantics ../implementation ../metatheory ../compiler/proofs
+INCLUDES=.. ../semantics ../implementation ../metatheory ../compiler ../compiler/proofs
 OPTIONS = QUIT_ON_FAILURE
 
 ifdef POLY

commit 40a3ab6b15dadd2262d30ce3cec7a56d4c437866

make compiler and subdirs build again
This is fallout from 02c127e25, which was a bigger change than I
realised. Have now moved the stuff to do with labels out into a separate
labels.lem.

Confusingly, the bytecode subirectory is logically the parent of
everything under the compiler directory.

compiler/Holmakefile
@@ -20,7 +20,7 @@ HOLHEAP = heap
 PARENT_HOLHEAP = ../semantics/sem_heap
 POLY_CLEANS = $(HOLHEAP) $(HOLHEAP).o
 
-BARE_THYS = ../semantics/terminationTheory ./bytecode/bytecodeTerminationTheory
+BARE_THYS = ../semantics/terminationTheory ./bytecode/bytecodeExtraTheory
 DEPS = $(patsubst %,%.uo,$(BARE_THYS))
 
 $(HOLHEAP): $(DEPS)

compiler/ToBytecodeScript.sml
@@ -491,49 +491,5 @@ val _ = Lib.with_flag (computeLib.auto_import_definitions, false) Defn.save_defn
   let s = ( FOLDL (cce_aux menv) s ( MAP SND (free_labs 0 e))) in
   emit s [Label l]))`;
 
-
-(* replace labels in bytecode with addresses *)
-
- val calculate_labels_defn = Hol_defn "calculate_labels" `
-
-(calculate_labels _ m n a [] = (m,n,a))
-/\
-(calculate_labels il m n a (Label l::lbc) =  
-(calculate_labels il ( FUPDATE  m ( l, n)) n a lbc))
-/\
-(calculate_labels il m n a (i::lbc) =  
-(calculate_labels il m (n + il i + 1) (i ::a) lbc))`;
-
-val _ = Lib.with_flag (computeLib.auto_import_definitions, false) Defn.save_defn calculate_labels_defn;
-
- val replace_labels_defn = Hol_defn "replace_labels" `
-
-(replace_labels _ a [] = a)
-/\
-(replace_labels m a (Jump (Lab l)::bc) =  
-(replace_labels m (Jump (Addr (fapply 0 l m)) ::a) bc))
-/\
-(replace_labels m a (JumpIf (Lab l)::bc) =  
-(replace_labels m (JumpIf (Addr (fapply 0 l m)) ::a) bc))
-/\
-(replace_labels m a (Call (Lab l)::bc) =  
-(replace_labels m (Call (Addr (fapply 0 l m)) ::a) bc))
-/\
-(replace_labels m a (PushPtr (Lab l)::bc) =  
-(replace_labels m (PushPtr (Addr (fapply 0 l m)) ::a) bc))
-/\
-(replace_labels m a (i::bc) =  
-(replace_labels m (i ::a) bc))`;
-
-val _ = Lib.with_flag (computeLib.auto_import_definitions, false) Defn.save_defn replace_labels_defn;
-
- val compile_labels_def = Define `
-
-(compile_labels il lbc = 
-  ((case calculate_labels il FEMPTY 0 [] lbc of
-       (m,_,bc) =>
-   replace_labels m [] bc
-   )))`;
-
 val _ = export_theory()
 

compiler/bytecode/Holmakefile
@@ -1,9 +1,9 @@
-INCLUDES = ../../semantics ..
+INCLUDES = ../../semantics
 
 OPTIONS = QUIT_ON_FAILURE
 
-BytecodeScript.sml: hol_lib.lem compilerLib.lem printer.lem bytecode.lem
-	lem -extra_level auto -hol_lib ../../semantics/hol_lib.lem $(patsubst %,-i ../../semantics/%.lem,lib ast semanticPrimitives) -hol_lib hol_lib.lem -hol compilerLib.lem -hol printer.lem -hol bytecode.lem &&\
-	sed -i 's/LEAST/$$LEAST/g' $@ &&\
-	sed -i 's/Hol.char/char/g' $@
-EXTRA_CLEANS = CompilerLibScript.sml PrinterScript.sml BytecodeScript.sml
+LabelsScript.sml: hol_lib.lem compilerLib.lem printer.lem bytecode.lem labels.lem
+	lem -extra_level auto -hol_lib ../../semantics/hol_lib.lem $(patsubst %,-i ../../semantics/%.lem,lib ast semanticPrimitives) -hol_lib hol_lib.lem -hol compilerLib.lem -hol printer.lem -hol bytecode.lem -hol labels.lem &&\
+	sed -i 's/LEAST/$$LEAST/g' BytecodeScript.sml &&\
+	sed -i 's/Hol.char/char/g' BytecodeScript.sml
+EXTRA_CLEANS = CompilerLibScript.sml PrinterScript.sml BytecodeScript.sml LabelsScript.sml

compiler/bytecode/LabelsScript.sml
@@ -1,0 +1,61 @@
+(*Generated by Lem from labels.lem.*)
+open bossLib Theory Parse res_quanTheory
+open fixedPointTheory finite_mapTheory listTheory pairTheory pred_setTheory
+open integerTheory set_relationTheory sortingTheory stringTheory wordsTheory
+
+val _ = numLib.prefer_num();
+
+
+
+open BytecodeTheory PrinterTheory CompilerLibTheory SemanticPrimitivesTheory AstTheory LibTheory
+
+val _ = new_theory "Labels"
+
+(*open CompilerLib*)
+(*open Bytecode*)
+
+(* replace labels in bytecode with addresses *)
+
+ val calculate_labels_defn = Hol_defn "calculate_labels" `
+
+(calculate_labels _ m n a [] = (m,n,a))
+/\
+(calculate_labels il m n a (Label l::lbc) =  
+(calculate_labels il ( FUPDATE  m ( l, n)) n a lbc))
+/\
+(calculate_labels il m n a (i::lbc) =  
+(calculate_labels il m (n + il i + 1) (i ::a) lbc))`;
+
+val _ = Lib.with_flag (computeLib.auto_import_definitions, false) Defn.save_defn calculate_labels_defn;
+
+ val replace_labels_defn = Hol_defn "replace_labels" `
+
+(replace_labels _ a [] = a)
+/\
+(replace_labels m a (Jump (Lab l)::bc) =  
+(replace_labels m (Jump (Addr (fapply 0 l m)) ::a) bc))
+/\
+(replace_labels m a (JumpIf (Lab l)::bc) =  
+(replace_labels m (JumpIf (Addr (fapply 0 l m)) ::a) bc))
+/\
+(replace_labels m a (Call (Lab l)::bc) =  
+(replace_labels m (Call (Addr (fapply 0 l m)) ::a) bc))
+/\
+(replace_labels m a (PushPtr (Lab l)::bc) =  
+(replace_labels m (PushPtr (Addr (fapply 0 l m)) ::a) bc))
+/\
+(replace_labels m a (i::bc) =  
+(replace_labels m (i ::a) bc))`;
+
+val _ = Lib.with_flag (computeLib.auto_import_definitions, false) Defn.save_defn replace_labels_defn;
+
+ val compile_labels_def = Define `
+
+(compile_labels il lbc = 
+  ((case calculate_labels il FEMPTY 0 [] lbc of
+       (m,_,bc) =>
+   replace_labels m [] bc
+   )))`;
+
+val _ = export_theory()
+

compiler/bytecode/bytecodeTerminationScript.sml
@@ -1,5 +1,11 @@
-open HolKernel boolLib bossLib BytecodeTheory
+open HolKernel Defn boolLib bossLib BytecodeTheory LabelsTheory lcsymtacs
 val _ = new_theory"bytecodeTermination"
+
+fun register name (def,ind) = let
+  val _ = save_thm(name^"_def", def)
+  val _ = save_thm(name^"_ind", ind)
+  val _ = computeLib.add_persistent_funs [name^"_def"]
+in (def,ind) end
 
 val _ = save_thm ("bc_fetch_aux_def", bc_fetch_aux_def);
 val _ = export_rewrites["bc_fetch_aux_def"];
@@ -23,5 +29,15 @@ val _ = export_rewrites["is_Label_def","bool_to_tag_def","bool_to_val_def",
                         "unit_tag_def","unit_val_def","closure_tag_def",
                         "block_tag_def"];
 val _ = Parse.overload_on("next_addr", ``λil ls. SUM (MAP (SUC o il) (FILTER ($~ o is_Label) ls))``)
+
+val _ = register "calculate_labels" (
+  tprove_no_defn ((calculate_labels_def,calculate_labels_ind),
+  WF_REL_TAC `measure (LENGTH o SND o SND o SND o SND)` >> rw[]))
+
+val _ = register "replace_labels" (
+  tprove_no_defn ((replace_labels_def,replace_labels_ind),
+  WF_REL_TAC `measure (LENGTH o SND o SND)` >> rw[]))
+
+val _ = export_rewrites["CompilerLib.the_def","CompilerLib.fapply_def"]
 
 val _ = export_theory()

compiler/bytecode/compileLabelsScript.sml
@@ -1,5 +1,5 @@
 open HolKernel bossLib boolLib boolSimps listTheory rich_listTheory alistTheory finite_mapTheory relationTheory lcsymtacs
-open miscLib miscTheory bytecodeTerminationTheory ToBytecodeTheory compilerTerminationTheory bytecodeEvalTheory bytecodeExtraTheory
+open miscLib miscTheory bytecodeTerminationTheory bytecodeEvalTheory bytecodeExtraTheory
 val _ = numLib.prefer_num()
 val _ = new_theory"compileLabels"
 
@@ -281,7 +281,7 @@ val bc_next_compile_labels = store_thm("bc_next_compile_labels",
        ALL_DISTINCT (FILTER is_Label s1.code)) ⇒
       let c = compile_labels s1.inst_length s1.code in
       bc_next (s1 with <| code := c |>) (s2 with <| code := c |>)``,
-  rw[compile_labels_def,replace_labels_thm] >>
+  rw[LabelsTheory.compile_labels_def,replace_labels_thm] >>
   imp_res_tac calculate_labels_thm >>
   rpt BasicProvers.VAR_EQ_TAC >>
   qunabbrev_tac`c` >>
@@ -347,7 +347,7 @@ val replace_labels_addrs_only = store_thm("replace_labels_addrs_only",
 
 val addrs_only_compile_labels = store_thm("addrs_only_compile_labels",
   ``EVERY addrs_only (compile_labels il ls)``,
-  rw[compile_labels_def] >>
+  rw[LabelsTheory.compile_labels_def] >>
   BasicProvers.EVERY_CASE_TAC >>
   rw[replace_labels_addrs_only])
 

compiler/bytecode/labels.lem
@@ -1,0 +1,36 @@
+open CompilerLib
+open Bytecode
+
+(* replace labels in bytecode with addresses *)
+
+let rec
+calculate_labels _ m n a [] = (m,n,a)
+and
+calculate_labels il m n a (Label l::lbc) =
+  calculate_labels il (Pmap.add l n m) n a lbc
+and
+calculate_labels il m n a (i::lbc) =
+  calculate_labels il m (n + il i + 1) (i::a) lbc
+
+let rec
+replace_labels _ a [] = a
+and
+replace_labels m a (Jump (Lab l)::bc) =
+  replace_labels m (Jump (Addr (fapply 0 l m))::a) bc
+and
+replace_labels m a (JumpIf (Lab l)::bc) =
+  replace_labels m (JumpIf (Addr (fapply 0 l m))::a) bc
+and
+replace_labels m a (Call (Lab l)::bc) =
+  replace_labels m (Call (Addr (fapply 0 l m))::a) bc
+and
+replace_labels m a (PushPtr (Lab l)::bc) =
+  replace_labels m (PushPtr (Addr (fapply 0 l m))::a) bc
+and
+replace_labels m a (i::bc) =
+  replace_labels m (i::a) bc
+
+let rec
+compile_labels il lbc =
+  let (m,_,bc) = calculate_labels il Pmap.empty 0 [] lbc in
+    replace_labels m [] bc

compiler/bytecode/miscScript.sml
@@ -5,7 +5,7 @@ val _ = new_theory "misc"
 (* TODO: move/categorize *)
 
 val PERM_PART = store_thm("PERM_PART",
-  ``∀P L l1 l2 p q. (p,q) = PART P L l1 l2 ⇒ PERM (L ++ (l1 ++ l2)) (p++q)``,
+  ``∀P L l1 l2 p q. ((p,q) = PART P L l1 l2) ⇒ PERM (L ++ (l1 ++ l2)) (p++q)``,
   GEN_TAC THEN Induct >>
   simp[PART_DEF] >> rw[] >- (
     first_x_assum(qspecl_then[`h::l1`,`l2`,`p`,`q`]mp_tac) >>

compiler/compilerTerminationScript.sml
@@ -242,14 +242,6 @@ val (bv_to_ov_def,bv_to_ov_ind) = register "bv_to_ov" (
   Q.ISPEC_THEN `bc_value_size` imp_res_tac SUM_MAP_MEM_bound >>
   srw_tac[ARITH_ss][]))
 
-val _ = register "calculate_labels" (
-  tprove_no_defn ((calculate_labels_def,calculate_labels_ind),
-  WF_REL_TAC `measure (LENGTH o SND o SND o SND o SND)` >> rw[]))
-
-val _ = register "replace_labels" (
-  tprove_no_defn ((replace_labels_def,replace_labels_ind),
-  WF_REL_TAC `measure (LENGTH o SND o SND)` >> rw[]))
-
 (*
 val _ = register "ov_to_string" (
   tprove_no_defn((ov_to_string_def,ov_to_string_ind),
@@ -279,7 +271,6 @@ val _ = export_rewrites
 ,"free_labs_def","no_labs_def","all_labs_def"
 ,"IntLang.CDiv_excv_def","IntLang.CBind_excv_def"
 ,"IntLang.CDiv_exc_def","IntLang.CBind_exc_def"
-,"ToIntLang.opn_to_prim2_def"
-,"CompilerLib.the_def","CompilerLib.fapply_def"];
+,"ToIntLang.opn_to_prim2_def"];
 
 val _ = export_theory()

compiler/proofs/Holmakefile
@@ -6,7 +6,7 @@ HOLHEAP = heap
 PARENT_HOLHEAP = ../heap
 EXTRA_CLEANS = $(HOLHEAP) $(HOLHEAP).o
 
-BARE_THYS = ../../semantics/terminationTheory ../compilerTerminationTheory ../bytecode/bytecodeExtraTheory ../miscTheory ../../metatheory/bigClockTheory
+BARE_THYS = ../../semantics/terminationTheory ../compilerTerminationTheory ../bytecode/bytecodeExtraTheory ../../metatheory/bigClockTheory
 DEPS = $(patsubst %,%.uo,$(BARE_THYS))
 
 $(HOLHEAP): $(DEPS)

compiler/toBytecode.lem
@@ -421,37 +421,3 @@ compile_code_env menv s e =
   let s = emit s [Jump (Lab l)] in
   let s = List.fold_left (cce_aux menv) s (List.map snd (free_labs 0 e)) in
   emit s [Label l]
-
-(* replace labels in bytecode with addresses *)
-
-let rec
-calculate_labels _ m n a [] = (m,n,a)
-and
-calculate_labels il m n a (Label l::lbc) =
-  calculate_labels il (Pmap.add l n m) n a lbc
-and
-calculate_labels il m n a (i::lbc) =
-  calculate_labels il m (n + il i + 1) (i::a) lbc
-
-let rec
-replace_labels _ a [] = a
-and
-replace_labels m a (Jump (Lab l)::bc) =
-  replace_labels m (Jump (Addr (fapply 0 l m))::a) bc
-and
-replace_labels m a (JumpIf (Lab l)::bc) =
-  replace_labels m (JumpIf (Addr (fapply 0 l m))::a) bc
-and
-replace_labels m a (Call (Lab l)::bc) =
-  replace_labels m (Call (Addr (fapply 0 l m))::a) bc
-and
-replace_labels m a (PushPtr (Lab l)::bc) =
-  replace_labels m (PushPtr (Addr (fapply 0 l m))::a) bc
-and
-replace_labels m a (i::bc) =
-  replace_labels m (i::a) bc
-
-let rec
-compile_labels il lbc =
-  let (m,_,bc) = calculate_labels il Pmap.empty 0 [] lbc in
-    replace_labels m [] bc

impl_proofs/Holmakefile
@@ -1,4 +1,4 @@
-INCLUDES=.. ../semantics ../implementation ../metatheory ../compiler ../compiler/proofs
+INCLUDES=.. ../semantics ../implementation ../metatheory ../compiler/bytecode ../compiler/proofs
 OPTIONS = QUIT_ON_FAILURE
 
 ifdef POLY

compiler/tests/bytecode_emitScript.sml
@@ -1,5 +1,5 @@
 open HolKernel bossLib boolLib EmitTeX basis_emitTheory
-open CompilerLibTheory PrinterTheory BytecodeTheory bytecodeTerminationTheory bytecodeEvalTheory
+open CompilerLibTheory PrinterTheory BytecodeTheory LabelsTheory bytecodeTerminationTheory bytecodeEvalTheory
 val _ = new_theory "bytecode_emit"
 
 val _ = Parse.temp_type_abbrev("string",``:char list``)
@@ -39,6 +39,7 @@ val CONCAT_RULE = PURE_REWRITE_RULE[mk_thm([],mk_eq(``FLAT:string list -> string
 
 val defs = map EmitML.DEFN [
 optionTheory.OPTION_BIND_def,
+fapply_def,
 i0_def,
 string_of_int_def,
 SemanticPrimitivesTheory.id_to_string_def,
@@ -54,7 +55,11 @@ bv_to_ov_def,
 bc_eval_stack_def,
 CONCAT_RULE(CONV_RULE(PURE_REWRITE_CONV[mk_thm([],mk_eq(``CONS:char -> string -> string``,``STRING``))]) bc_eval1_def),
 bc_eval_compute,
-init_bc_state_def]
+init_bc_state_def
+, calculate_labels_def
+, replace_labels_def
+, compile_labels_def
+]
 
 val _ = EmitML.eSML "bytecode" (
   (EmitML.OPEN ["int","fmap","string"])

compiler/tests/compile_emitScript.sml
@@ -79,7 +79,6 @@ val defs = map EmitML.DEFN
 , lunion_def
 , lshift_def
 , el_check_def
-, fapply_def
 , free_vars_def
 , emit_def
 , i1_def
@@ -109,9 +108,6 @@ val defs = map EmitML.DEFN
 , free_labs_def
 , cce_aux_def
 , compile_code_env_def
-, calculate_labels_def
-, replace_labels_def
-, compile_labels_def
 , IntLangTheory.tuple_cn_def
 , IntLangTheory.bind_exc_cn_def
 , IntLangTheory.div_exc_cn_def