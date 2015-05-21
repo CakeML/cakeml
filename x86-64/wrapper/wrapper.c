928e0008ff5df8c9428bbfcc598da44ea847144e
Magnus Myreen <magnus.myreen@cl.cam.ac.uk>
2015-05-21 14:48:21 +0100

    wrapper.c fixed (thanks Stephen Kell!)

--- x86-64/wrapper/wrapper.c
+++ x86-64/wrapper/wrapper.c
@@ -5,14 +5,34 @@
 const long long HEAP_SIZE = 1024 * 1024 * 1024;
 const long long CODE_HEAP_SIZE = 1024 * 1024 * 1024;
 
+char * alloc_executable(size_t len) { /* len = number of bytes */
+  /* Local variables */
+  size_t page_size = getpagesize();
+  /* Round up so that len is a multiple of getpagesize() */
+  if (len % page_size) {
+    len &= ~(page_size-1);
+    len += page_size;
+  }
+  /* Open a file for use as scratch memory */
+  int fd = 0, ret = 0;
+  void *pa = MAP_FAILED;
+  char template[] = "/tmp/alloc_executable_XXXXXX";
+  fd = mkstemp(template);
+  if (fd < 0) return pa;
+  unlink(template);
+  ret = ftruncate(fd, len);
+  if (ret < 0) return pa;
+  /* Do the allocation */
+  return mmap(NULL, len, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_PRIVATE, fd, 0);
+}
+
 int main(int argc, char** argv) {
   /* allocate memory -- code heap must have EXEC rights */
-  void *codeheap = malloc(CODE_HEAP_SIZE);
-  mprotect(codeheap, CODE_HEAP_SIZE, PROT_READ|PROT_WRITE|PROT_EXEC);
+  char *codeheap = alloc_executable(CODE_HEAP_SIZE);
   unsigned long long *heap = malloc(HEAP_SIZE + 16);
   /* start verified code */
   heap[2] = HEAP_SIZE;
-  heap[3] = (long) (& codeheap);
+  heap[3] = (long) codeheap;
   heap[4] = CODE_HEAP_SIZE;
   heap[5] = (long) (& putchar);
   heap[6] = (long) (& getchar);
