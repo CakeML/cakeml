4c8f3da2878bab6e6fc5aada64c1bfedd23ba08e
laMudri <lamudri@gmail.com>
2015-07-02 17:25:37 +0100

    switched to using type-annotated parse tree from OCaml

--- unverified/ocaml-syntax/typedOCamlToCakeML.ml
+++ unverified/ocaml-syntax/typedOCamlToCakeML.ml
@@ -6,8 +6,7 @@ open BatResult.Monad
 
 open Asttypes
 open Longident
-open Parsetree
-open Parsing
+open Typedtree
 
 let rec mapM f = function
   | [] -> return []
@@ -15,54 +14,53 @@ let rec mapM f = function
                  mapM f xs >>= fun ys ->
                  return (y :: ys)
 
-let print_value_binding pvb =
-  ignore pvb.pvb_pat;
+let print_value_binding vb =
   Bad "Value bindings not supported."
 
 let paren s = "(" ^ s ^ ")"
 
 let fix_type_name x = x
 
-let rec print_core_type ptyp =
-  match ptyp.ptyp_desc with
-  | Ptyp_any -> return "_"
-  | Ptyp_var name -> return @@ "'" ^ fix_type_name name
-  | Ptyp_arrow (l, dom, cod) ->
+let rec print_longident = function
+  | Lident s -> return s
+  | Ldot (t, s) -> print_longident t >>= fun ct ->
+                   Bad "I don't know what this feature is. Please tell me."
+  | Lapply (a, b) -> print_longident a >>= fun aname ->
+                     print_longident b >>= fun bname ->
+                     return @@ paren (aname ^ " " ^ bname)
+
+let rec print_core_type ctyp =
+  match ctyp.ctyp_desc with
+  | Ttyp_any -> return "_"
+  | Ttyp_var name -> return @@ "'" ^ fix_type_name name
+  | Ttyp_arrow (l, dom, cod) ->
     print_core_type dom >>= fun a ->
     print_core_type cod >>= fun b ->
     return @@ paren ((if l = "" then l ^ ":" else "") ^ a ^ " -> " ^ b)
     (* ^^ label type changes in 4.03 ^^ *)
-  | Ptyp_tuple ts -> print_ptyp_tuple ts >>= fun t ->
+  | Ttyp_tuple ts -> print_ttyp_tuple ts >>= fun t ->
                      return @@ paren t
-  | Ptyp_constr (lid, ts) -> print_longident lid >>= fun name ->
-                             return @@ name
+  | Ttyp_constr (path, lid, ts) -> print_longident lid.txt >>= fun name ->
+                                   return @@ name
   | _ -> Bad "Some core types syntax not supported."
 
-and print_ptyp_tuple = function
+and print_ttyp_tuple = function
   | [] -> return ""
   | [t] -> print_core_type t
   | t :: ts -> print_core_type t >>= fun core_type ->
-                 print_ptyp_tuple ts >>= fun rest ->
+                 print_ttyp_tuple ts >>= fun rest ->
                  return @@ core_type ^ " * " ^ rest
 
-and print_longident = function
-  | Lident s -> s
-  | Ldot (t, s) -> print_core_type t >>= fun ct ->
-                   Bad "I don't know what this feature is. Please tell me."
-  | Lapply (a, b) -> print_core_type a >>= fun aname ->
-                     print_core_type b >>= fun bname ->
-                     return @@ paren (aname ^ " " ^ bname)
-
-let print_ptype_params = function
+let print_typ_params = function
   | [] -> return ""
-  | [(ptyp, _)] -> print_core_type ptyp >>= fun core_type ->
+  | [(typ, _)] -> print_core_type typ >>= fun core_type ->
                    return @@ " " ^ core_type
   | xs ->
     let rec f = function
       | [] -> Bad "How did we get here?"
-      | [ptyp, _] -> print_core_type ptyp >>= fun core_type ->
+      | [typ, _] -> print_core_type typ >>= fun core_type ->
                        return @@ core_type ^ " )"
-      | (ptyp, _) :: ys -> print_core_type ptyp >>= fun core_type ->
+      | (typ, _) :: ys -> print_core_type typ >>= fun core_type ->
                            f ys >>= fun rest ->
                            return @@ core_type ^ " , " ^ rest
     in f xs >>= fun rest ->
@@ -72,17 +70,17 @@ let print_ptype_params = function
    support for value constructors for record types was added. *)
 
 (*let print_constructor_arguments = function
-  | Pcstr_tuple ts -> print_ptyp_tuple ts
+  | Pcstr_tuple ts -> print_ttyp_tuple ts
   | Pcstr_record ds -> Bad "Record syntax not supported."*)
 
 let print_constructor_declaration decl =
-  (* Replace `print_ptyp_tuple` with `print_constructor_arguments
+  (* Replace `print_ttyp_tuple` with `print_constructor_arguments
      in OCaml 4.03. *)
-  print_ptyp_tuple decl.pcd_args >>= fun constructor_args ->
-  return @@ decl.pcd_name.txt ^
+  print_ttyp_tuple decl.cd_args >>= fun constructor_args ->
+  return @@ decl.cd_name.txt ^
     if constructor_args = "" then "" else " of " ^ constructor_args
 
-let print_ptype_variant =
+let print_ttyp_variant =
   let rec f = function
   | [] -> return ""
   | d :: ds -> print_constructor_declaration d >>= fun constructor_decl ->
@@ -94,32 +92,32 @@ let print_ptype_variant =
                f ds >>= fun rest ->
                return @@ "\n  " ^ constructor_decl ^ rest
 
-let print_type_declaration ptype =
-  let keyword = match ptype.ptype_kind with
-                | Ptype_abstract -> "type"
+let print_type_declaration typ =
+  let keyword = match typ.typ_kind with
+                | Ttype_abstract -> "type"
                 | _ -> "datatype"
   in
-  print_ptype_params ptype.ptype_params >>= fun params ->
-  (match ptype.ptype_manifest, ptype.ptype_kind with
-  | Some m, Ptype_abstract -> print_core_type m >>= fun manifest ->
+  print_typ_params typ.typ_params >>= fun params ->
+  (match typ.typ_manifest, typ.typ_kind with
+  | Some m, Ttype_abstract -> print_core_type m >>= fun manifest ->
                               return @@ " = " ^ manifest
-  | Some m, Ptype_variant ds -> print_core_type m >>= fun manifest ->
+  | Some m, Ttype_variant ds -> print_core_type m >>= fun manifest ->
                                 return @@ " = datatype " ^ manifest
-  | None, Ptype_abstract -> return ""
-  | None, Ptype_variant ds -> print_ptype_variant ds >>= fun expr ->
+  | None, Ttype_abstract -> return ""
+  | None, Ttype_variant ds -> print_ttyp_variant ds >>= fun expr ->
                               return @@ " =" ^ expr
   | _ -> Bad "Type of type declaration not supported."
   ) >>= fun rest ->
-  return @@ keyword ^ params ^ " " ^ fix_type_name ptype.ptype_name.txt ^ rest
+  return @@ keyword ^ params ^ " " ^ fix_type_name typ.typ_name.txt ^ rest
 
 
-let print_structure_item pstr =
-  match pstr.pstr_desc with
-  | Pstr_value (_, bs) ->
+let print_structure_item str =
+  match str.str_desc with
+  | Tstr_value (_, bs) ->
     mapM (fun b -> print_value_binding b >>= fun x ->
                    return (x ^ ";\n")) bs >>= fun ss ->
     return @@ fold_right (^) ss ""
-  | Pstr_type ds ->
+  | Tstr_type ds ->
     mapM (fun d -> print_type_declaration d >>= fun x ->
                    return (x ^ ";\n")) ds >>= fun ss ->
     return @@ fold_right (^) ss ""
@@ -131,4 +129,6 @@ let print_result = function
 
 let lexbuf = Lexing.from_channel stdin
 let parsetree = Parse.implementation lexbuf
-let _ = map (print_result % print_structure_item) parsetree
+let typedtree, signature, env =
+  Typemod.type_structure Env.empty parsetree Location.none
+let _ = map (print_result % print_structure_item) typedtree.str_items
