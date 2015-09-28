39db83376d65b0fba5b174afbff71165b56742ac
Ramana Kumar <ramana@member.fsf.org>
2015-09-28 15:27:26 +1000

    remove apparently unnecessary quotes

--- developers/build-email.sh
+++ developers/build-email.sh
@@ -11,17 +11,17 @@ timeout 8h developers/regression-test.sh &>$tmpfile
 
 case $? in
   124)
-    cat $tmpfile | mail -S from="'$from'" -s "TIMEOUT" $to
+    cat $tmpfile | mail -S from="$from" -s "TIMEOUT" $to
     echo "build timed out"
     ;;
   0)
-    cat $tmpfile | mail -S from="'$from'" -s "OK" $to
+    cat $tmpfile | mail -S from="$from" -s "OK" $to
     echo "build succeeded"
     ;;
   *)
     subject=$(tail -n1 $tmpfile)
     cd $(echo $subject | cut -f2 -d' ')
-    cat timing.log <(tail -n80 regression.log) | col -bx | mail -S from="'$from'" -s "'$subject'"  -q $tmpfile $to
+    cat timing.log <(tail -n80 regression.log) | col -bx | mail -S from="$from" -s "$subject"  -q $tmpfile $to
     echo "build failed"
     ;;
 esac
