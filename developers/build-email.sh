383e1dad5b33dff84710674f719bff98743cbd74
Ramana Kumar <ramana@member.fsf.org>
2015-10-16 09:18:28 +1100

    reduce use of options to mail
    For greater compatibility with various different versions of the
    program.

--- developers/build-email.sh
+++ developers/build-email.sh
@@ -4,24 +4,23 @@ cd $(dirname "$0")/..
 
 tmpfile=/tmp/vml-build-email.txt
 
-from='"CakeML Builds" <builds@cakeml.org>'
 to='builds@cakeml.org'
 
 timeout 8h developers/regression-test.sh &>$tmpfile
 
 case $? in
   124)
-    cat $tmpfile | mail -S from="$from" -s "TIMEOUT" $to
+    cat $tmpfile | mail -s "TIMEOUT" $to
     echo "build timed out"
     ;;
   0)
-    cat $tmpfile | mail -S from="$from" -s "OK" $to
+    cat $tmpfile | mail -s "OK" $to
     echo "build succeeded"
     ;;
   *)
     subject=$(tail -n1 $tmpfile)
     cd $(echo $subject | cut -f2 -d' ')
-    cat timing.log <(tail -n80 regression.log) | col -bx | mail -S from="$from" -s "$subject"  -q $tmpfile $to
+    cat $tmpfile timing.log <(tail -n80 regression.log) | col -bx | mail -s "$subject" $to
     echo "build failed"
     ;;
 esac
