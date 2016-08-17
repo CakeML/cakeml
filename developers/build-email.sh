53a2a3746fd8cd7d85c394e376748dc377b356ae
Ramana Kumar <ramana@member.fsf.org>
2016-08-18 07:18:03 +1000

    Backport Bamboo-related tweaks to build-email.sh
    Looking to get an automated build test for version1.

--- developers/build-email.sh
+++ developers/build-email.sh
@@ -2,16 +2,18 @@
 
 cd $(dirname "$0")/..
 
-tmpfile=/tmp/vml-build-email.txt
+tmpdir=${1:-"/tmp"}
+tmpfile="$tmpdir/vml-build-email.txt"
 
 to='builds@cakeml.org'
 
-timeout 8h developers/regression-test.sh &>$tmpfile
+timeout 8h developers/regression-test.sh &> >(tee $tmpfile)
 
 case $? in
   124)
     cat $tmpfile | mail -s "TIMEOUT" $to
     echo "build timed out"
+    exit 124
     ;;
   0)
     cat $tmpfile | mail -s "OK" $to
@@ -22,5 +24,6 @@ case $? in
     cd $(echo $subject | cut -f2 -d' ')
     cat $tmpfile timing.log <(tail -n80 regression.log) | col -bx | mail -s "$subject" $to
     echo "build failed"
+    exit 1
     ;;
 esac
