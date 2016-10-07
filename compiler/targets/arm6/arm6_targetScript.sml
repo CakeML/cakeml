CakeML/cakeml

commit 7fa7d74480dd1504af7eabb3f14d1770fcd12ddd

Attempt to update register naming for all targets
Principles:
- avoid stack pointer
- ensure top three registers are callee-saved
- map 0, 1, 2 to link-reg, 1st arg, 2nd arg
- minimise renames otherwise

On MIPS there are apparently two reserved-for-OS-kernel registers, so I
added those to the avoids too. Not sure if this is necessary.

No archs mention "base pointer" (apart from x86), so I haven't added any
avoids for that. However, maybe "frame pointer" on other archs needs to
be avoided? Not sure.

Most archs provide more than 2 argument registers. Not sure if it's
worthwhile assigning more than 2 of them correctly. cake_main takes 4
args, so maybe 4 should be named according to convention...

compiler/targets/arm6/arm6_targetScript.sml
@@ -156,7 +156,7 @@ val arm6_config_def = Define`
     ; encode := arm6_enc
     ; code_alignment := 2
     ; reg_count := 16
-    ; avoid_regs := [15]
+    ; avoid_regs := [13;15]
     ; link_reg := SOME 14
     ; has_mem_32 := F
     ; two_reg_arith := F

compiler/targets/mips/mips_targetScript.sml
@@ -174,7 +174,7 @@ val mips_config_def = Define`
    <| ISA := MIPS
     ; encode := mips_enc
     ; reg_count := 32
-    ; avoid_regs := [0; 1]
+    ; avoid_regs := [0; 1; 26; 27; 29]
     ; link_reg := SOME 31
     ; has_mem_32 := T
     ; two_reg_arith := F

compiler/backend/stack_namesScript.sml
@@ -105,25 +105,21 @@ val x64_names_def = save_thm("x64_names_def",
 
 val arm_names_def = Define `
   arm_names =
-    (* source can use 14 regs (0-13),
-       target's r13 must be avoided,
-       source 0 must represent r14 (link register) *)
+    (* source can use 14 regs,
+       target's r15 must be avoided (pc),
+       target's r13 must be avoided (stack pointer),
+       source 0 must represent r14 (link register),
+       source 1-2 must be r0 and r1 (1st 2 arguments)
+       the top three (source 11-13) must be callee-saved
+       (callee-saved include: r4-r8, r10-11) *)
     (insert 0 14 o
      insert 1 0 o
      insert 2 1 o
-     insert 3 2 o
-     insert 4 3 o
-     insert 5 4 o
-     insert 6 5 o
-     insert 7 6 o
-     insert 8 7 o
-     insert 9 8 o
-     insert 10 9 o
-     insert 11 10 o
-     insert 12 11 o
-     insert 13 12 o
+     insert 12 8 o
+     insert 13 10 o
      (* the rest just ensures that the mapping is well-formed *)
-     insert 14 13) LN:num num_map`
+     insert 8 2 o
+     insert 10 12) LN:num num_map`
 
 val arm_names_def = save_thm("arm_names_def",
   CONV_RULE (RAND_CONV EVAL) arm_names_def);
@@ -131,34 +127,41 @@ val arm_names_def = save_thm("arm_names_def",
 val arm8_names_def = Define `
   arm8_names =
     (* source can use 31 regs (0-30),
-       target's r31 must be avoided (hardcoded to 0, sometimes sp),
-       source 0 must represent r30 (link register) *)
+       target's r31 must be avoided (stack pointer),
+       source 0 must represent r30 (link register),
+       source 1-2 must be r0,r1 (1st 2 args),
+       top three (28-30) must be callee-saved (in r19-r29) *)
     (insert 0 30 o
      insert 1 0 o
      insert 2 1 o
-     insert 30 2) LN:num num_map`
+     insert 30 27 o
+     insert 27 2) LN:num num_map`
 
 val arm8_names_def = save_thm("arm8_names_def",
   CONV_RULE (RAND_CONV EVAL) arm8_names_def);
 
 val mips_names_def = Define `
   mips_names =
-    (* source can use 30 regs (2-31),
+    (* source can use 27 regs (r2-r25,r28,r30-r31),
        target's r0 must be avoided (hardcoded to 0),
        target's r1 must be avoided (used by encoder in asm),
-       source 0 must represent r31 (link register)
-       argument regs 4-7 *)
+       target's r29 must be avoided (stack pointer),
+       target's r26-r27 avoided (reserved for OS kernel),
+       source 0 must represent r31 (link register),
+       source 1 2 must be r4, r5 (1st 2 args),
+       top 3 (24-26) must be callee-saved (in 16-23, 28, 30) *)
     (insert 0 31 o
      insert 1 4 o
      insert 2 5 o
-     insert 3 6 o
-     insert 4 7 o
-     insert 5 2 o
-     insert 6 3 o
-     insert 7 30 o
+     insert 24 21 o
+     insert 25 22 o
+     insert 26 23 o
      (* the rest just ensures that the mapping is well-formed *)
-     insert 30 1 o
-     insert 31 0) LN:num num_map`
+     insert 4 2 o
+     insert 5 28 o
+     insert 21 24 o
+     insert 22 25 o
+     insert 23 30) LN:num num_map`
 
 val mips_names_def = save_thm("mips_names_def",
   CONV_RULE (RAND_CONV EVAL) mips_names_def);

commit 2708b95dcb580e2d4764f799d03c0d30e407cde3

Repair ARMv6 and MIPS proofs.
Broken by 7fa7d74.

compiler/targets/arm6/arm6_targetScript.sml
@@ -156,7 +156,7 @@ val arm6_config_def = Define`
     ; encode := arm6_enc
     ; code_alignment := 2
     ; reg_count := 16
-    ; avoid_regs := [13;15]
+    ; avoid_regs := [13; 15]
     ; link_reg := SOME 14
     ; has_mem_32 := F
     ; two_reg_arith := F
@@ -184,9 +184,7 @@ val arm6_target_def = Define`
     ; proj := arm6_proj
     |>`
 
-val (arm6_config, arm6_asm_ok) =
-  asmLib.target_asm_rwts [DECIDE ``a < 16 /\ a <> 15n = a < 15``]
-    ``arm6_config``
+val (arm6_config, arm6_asm_ok) = asmLib.target_asm_rwts [] ``arm6_config``
 
 val arm6_config = save_thm("arm6_config", arm6_config)
 val arm6_asm_ok = save_thm("arm6_asm_ok", arm6_asm_ok)

compiler/targets/arm6/proofs/arm6_targetProofScript.sml
@@ -18,7 +18,8 @@ val arm6_asm_ok =
   REWRITE_RULE [valid_immediate] arm6_targetTheory.arm6_asm_ok
 
 val lem1 = Q.prove(
-   `!n m. n < 15 ==> RName_PC <> R_mode m (n2w n) /\ n MOD 16 <> 15`,
+   `!n m. n < 16 /\ n <> 13 /\ n <> 15 ==>
+          RName_PC <> R_mode m (n2w n) /\ n MOD 16 <> 15`,
    CONV_TAC (Conv.ONCE_DEPTH_CONV SYM_CONV)
    \\ simp [arm_stepTheory.R_x_pc]
    )
@@ -35,11 +36,11 @@ val lem5 =
 
 val lem6 = Q.prove(
    `!s state c n.
-      target_state_rel arm6_target s state /\ n < 15 /\
+      target_state_rel arm6_target s state /\ n < 16 /\ n <> 13 /\ n <> 15 /\
       aligned 2 (c + s.regs n) ==>
       aligned 2 (c + state.REG (R_mode state.CPSR.M (n2w n)))`,
    rw [asmPropsTheory.target_state_rel_def, alignmentTheory.aligned_extract,
-       arm6_target_def, arm6_config_def]
+       arm6_target_def, arm6_config_def, lem1]
    )
 
 val lem7 = Q.prove(
@@ -581,8 +582,7 @@ in
          [asmPropsTheory.sym_target_state_rel, arm6_target_def,
           asmPropsTheory.all_pcs, arm6_ok_def, arm6_config,
           combinTheory.APPLY_UPDATE_THM, alignmentTheory.aligned_numeric,
-          alignmentTheory.align_aligned, set_sepTheory.fun2set_eq,
-          DECIDE ``a < 16 /\ a <> 15n = a < 15``]
+          alignmentTheory.align_aligned, set_sepTheory.fun2set_eq]
       \\ NO_STRIP_REV_FULL_SIMP_TAC (srw_ss()) []
       \\ REPEAT strip_tac
       \\ reg_tac

compiler/targets/mips/mips_targetScript.sml
@@ -207,10 +207,7 @@ val mips_target_def = Define`
     ; proj := mips_proj
     |>`
 
-val (mips_config, mips_asm_ok) =
-  asmLib.target_asm_rwts
-    [DECIDE ``a < 32 /\ a <> 0n /\ a <> 1n = 1 < a /\ a < 32``]
-    ``mips_config``
+val (mips_config, mips_asm_ok) = asmLib.target_asm_rwts [] ``mips_config``
 
 val mips_config = save_thm("mips_config", mips_config)
 val mips_asm_ok = save_thm("mips_asm_ok", mips_asm_ok)

compiler/targets/mips/proofs/mips_targetProofScript.sml
@@ -58,7 +58,7 @@ val lem4 =
 val lem5 = Q.prove(
    `!s state.
      target_state_rel mips_target s state ==>
-     !n. 1 < n /\ n < 32 ==>
+     !n. n < 32 /\ n <> 0 /\ n <> 1 /\ n <> 26 /\ n <> 27 /\ n <> 29 ==>
          (s.regs n = state.gpr (n2w n)) /\ n <> 0 /\ n2w n <> 1w : word5`,
    lrw [asmPropsTheory.target_state_rel_def, mips_target_def, mips_config_def]
    )
@@ -108,8 +108,6 @@ val lem10 =
                 c ' 10; c ' 9; c ' 8; c ' 7; c ' 6; c ' 5;
                 c ' 4; c ' 3; c ' 2; c ' 1; c ' 0]: word16) = c)``
 
-val lem11 = DECIDE ``!i. i < 32 /\ i <> 0 /\ i <> 1 = 1 < i /\ i < 32n``
-
 val lem12 = utilsLib.mk_cond_rand_thms [optionSyntax.is_some_tm]
 
 val adc_lem1 = Q.prove(
@@ -118,7 +116,7 @@ val adc_lem1 = Q.prove(
 
 val adc_lem2 = Q.prove(
   `!r2 : word64 r3 : word64.
-    (18446744073709551616 <= w2n r2 + (w2n r3 + 1) =
+    (18446744073709551616 <= w2n r2 + w2n r3 + 1 =
      18446744073709551616w <=+ w2w r2 + w2w r3 + 1w : 65 word) /\
     (18446744073709551616 <= w2n r2 + w2n r3 =
      18446744073709551616w <=+ w2w r2 + w2w r3 : 65 word)`,
@@ -229,17 +227,22 @@ end
 
 fun state_tac asm =
    NO_STRIP_FULL_SIMP_TAC (srw_ss())
-      [asmPropsTheory.all_pcs, mips_ok_def, lem11,
+      [asmPropsTheory.all_pcs, mips_ok_def,
        asmPropsTheory.sym_target_state_rel, mips_target_def, mips_config,
        alignmentTheory.aligned_numeric, set_sepTheory.fun2set_eq, lem8, lem9]
    \\ (if asmLib.isAddCarry asm then
          REPEAT strip_tac
          \\ Cases_on `i = n2`
-         \\ simp [combinTheory.UPDATE_APPLY, adc_lem1]
-         >- (Cases_on `r4 = 0w` \\ simp [adc_lem2] \\ blastLib.BBLAST_TAC)
+         \\ asm_simp_tac (srw_ss()) [combinTheory.UPDATE_APPLY, adc_lem1]
+         >- (Cases_on `r4 = 0w`
+             \\ asm_simp_tac (srw_ss()) [adc_lem2]
+             \\ blastLib.BBLAST_TAC
+            )
          \\ Cases_on `i = n`
-         \\ simp [combinTheory.UPDATE_APPLY, GSYM wordsTheory.word_add_n2w]
-         \\ rw [bitstringLib.v2w_n2w_CONV ``v2w [F] : word64``,
+         \\ asm_simp_tac (srw_ss())
+              [combinTheory.UPDATE_APPLY, GSYM wordsTheory.word_add_n2w]
+         \\ srw_tac []
+               [bitstringLib.v2w_n2w_CONV ``v2w [F] : word64``,
                 bitstringLib.v2w_n2w_CONV ``v2w [T] : word64``]
        else
          rw [combinTheory.APPLY_UPDATE_THM,