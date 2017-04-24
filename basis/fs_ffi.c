3f12810982fd51337b8eb8abfb218a674806286b
Hugo Férée <h.feree@kent.ac.uk>
2017-04-24 17:27:12 +0100

    C implementation of fsFFI

---
+++ basis/fs_ffi.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+FILE* infds[256];
+
+int nextFD() {
+  int fd = 0;
+  while(fd < 256 && infds[fd] != NULL) fd++;
+  return fd;
+}
+
+void ffiopen (char *a) {
+  int fd = nextFD();
+  if (fd < 255 && (infds[fd] = fopen(a,"r")))
+    a[0] = fd;
+  else
+    a[0] = 255;
+}
+
+void ffiopen_append (char *a) {
+  int fd = nextFD();
+  if (fd < 255 && (infds[fd] = fopen(a,"a")))
+    a[0] = fd;
+  else
+    a[0] = 255;
+}
+
+void ffiread (char *a) {
+  int nread = read(fileno(infds[a[0]]), &a[2], a[1]);
+  if(nread < 0) a[0] = 0;
+  else{
+    a[0] = 1; 
+    a[1] = nread;
+  }
+}
+
+void ffiwrite (char * a){
+  int nw = write(fileno(infds[a[0]]), &a[2], a[1]);  
+  if(nw < 0) a[0] = 0;
+  else{
+    a[0] = 1; 
+    a[1] = nw;
+  }
+}
+
+void fficlose (char *a) {
+  if (infds[a[0]] && fclose(infds[a[0]]) == 0) {
+    infds[a[0]] = NULL;
+    a[0] = 1;
+  }
+  else
+    a[0] = 0;
+}
+
+void ffiisEof (char *a) {
+  int c; /* not char, other EOF is mapped to a valid char */
+  if (infds[a[0]])
+    if ((c = fgetc(infds[a[0]])) == EOF)
+      a[0] = 1;
+    else {
+      ungetc(c, infds[a[0]]);
+      a[0] = 0;
+    }
+  else
+    a[0] = 255;
+}
