da7dece142a8664306f49ece0cb09ece59e3da32
Hugo Férée <h.feree@kent.ac.uk>
2017-10-04 17:27:57 +0100

    fs_ffi.c: change char to unsigned char to avoid cast from int issues           fix erroneous return bytes

--- basis/fs_ffi.c
+++ basis/fs_ffi.c
@@ -11,7 +11,7 @@ extern char **argv;
 
 #define MAXLEN 256
 
-void ffigetArgs (char *a) {
+void ffigetArgs (unsigned char *a) {
         int i, j, k;
 
         for (i = 0, k = 0; (i < argc) && (k < MAXLEN); i++, k++) {
@@ -32,9 +32,9 @@ int nextFD() {
   return fd;
 }
 
-void ffiopen_in (char *a) {
+void ffiopen_in (unsigned char *a) {
   int fd = nextFD();
-  if (fd < 255 && (infds[fd] = open(a, O_RDONLY|O_CREAT))){
+  if (fd <= 255 && (infds[fd] = open(a, O_RDONLY|O_CREAT))){
     a[0] = 0;
     a[1] = fd;
   }
@@ -42,9 +42,9 @@ void ffiopen_in (char *a) {
     a[0] = 255;
 }
 
-void ffiopen_out (char *a) {
+void ffiopen_out (unsigned char *a) {
   int fd = nextFD();
-  if (fd < 255 && (infds[fd] = open(a, O_RDWR|O_CREAT|O_TRUNC))){
+  if (fd <= 255 && (infds[fd] = open(a, O_RDWR|O_CREAT|O_TRUNC))){
     a[0] = 0;
     a[1] = fd;
   }
@@ -52,25 +52,30 @@ void ffiopen_out (char *a) {
     a[0] = 255;
 }
 
-void ffiread (char *a) {
+void ffiread (unsigned char *a) {
   int nread = read(infds[a[0]], &a[3], a[1]);
-  if(nread < 0) a[0] = 0;
+  if(nread < 0){
+    a[0] = 1;
+  }
   else{
-    a[0] = 1; 
+    a[0] = 0; 
     a[1] = nread;
   }
 }
 
-void ffiwrite (char * a){
+void ffiwrite (unsigned char * a){
   int nw = write(infds[a[0]], &a[3+a[2]], a[1]);
-  if(nw < 0) a[0] = 0;
+  if(nw < 0){
+      a[0] = 1;
+  }
+
   else{
-    a[0] = 1; 
+    a[0] = 0; 
     a[1] = nw;
   }
 }
 
-void fficlose (char *a) {
+void fficlose (unsigned char *a) {
   if (infds[a[0]] && close(infds[a[0]]) == 0) {
     infds[a[0]] = -1;
     a[0] = 1;
@@ -79,7 +84,7 @@ void fficlose (char *a) {
     a[0] = 0;
 }
 /*
-void ffiseek (char *a) {
+void ffiseek (unsigned char *a) {
   int off = lseek(infds[a[0]], a[2], SEEK_SET);
   if (off = -1)
     a[0] = 1;
