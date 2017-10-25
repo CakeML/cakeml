ef89a576eb148f93436636bacfdb54fe6885637a
Hugo Férée <h.feree@kent.ac.uk>
2017-10-25 14:14:21 +0100

    Build exercises with Holmake

--- tutorial/solutions/make_ex.sh
+++ tutorial/solutions/make_ex.sh
@@ -2,8 +2,9 @@
 
 # An answer block starts with a single commented line of the form
 # "(*ex ... *)" and is ended by a single line of the form (* ex*)
-
-sed 's/(\*ex \(.*\) \*)/\1\n (\*ex/g' "$1" \
-    | sed '/(\*ex/,/ex\*)/d'
-
+for file in "$@"
+do
+    sed 's/(\*ex \(.*\) \*)/\1\n (\*ex/g' "$file" \
+        | sed '/(\*ex/,/ex\*)/d' > "../$file"
+done
 
