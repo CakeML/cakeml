4270cf2cf913d30d8a8fd82c62bd71661377ceb5
zapashcanon <leo@ndrs.fr>
2017-12-06 15:08:40 +0100

    Improve scripts by fixing ShellCheck warnings

--- compiler/benchmarks/ocaml/time_all.sh
+++ compiler/benchmarks/ocaml/time_all.sh
@@ -1,16 +1,18 @@
+#!/bin/sh
+
 TIMEFORMAT=%R
 for benchmark in ocamlc_*; do
-  echo $benchmark
-  for i in `seq 30`
+  echo "$benchmark"
+  for _ in $(seq 30)
   do
-    time ./$benchmark
+    time "./$benchmark"
   done
 done
 
 for benchmark in ocamlopt_*; do
-  echo $benchmark
-  for i in `seq 30`
+  echo "$benchmark"
+  for _ in $(seq 30)
   do
-    time ./$benchmark
+    time "./$benchmark"
   done
 done
