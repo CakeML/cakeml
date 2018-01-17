62086c5949a24c5e2cff2aacd4b116eb8e23b6c8
Scott Owens <S.A.Owens@kent.ac.uk>
2018-01-17 00:26:09 +0000

    sptree benchmarks for CakeML and OCaml
    sptree :( super slow

---
+++ compiler/benchmarks/cakeml_benchmarks/ocaml/sptree_contain.ml
@@ -0,0 +1,132 @@
+(* NB, 6561 (3^8) and 40000 (2^7 * 5^5) are chosen to be relatively prime so
+ * that all element of the array are hit *)
+
+let even x = x mod 2 = 0
+
+  type 'a sptree_spt =  Bs of  'a sptree_spt *  'a  *  'a sptree_spt
+                         |  Bn of  'a sptree_spt *  'a sptree_spt
+                         |  Ls of  'a
+                     |  Ln
+
+
+  let rec lookup v7 v8 =
+    match  v8
+    with  Ln ->  None
+    |   Ls(v1) ->  (if  (v7 <= 0)
+                    then  (Some(v1))
+                    else  None)
+    |   Bn(v3,v2) ->  (if  (v7 <= 0)
+                       then  None
+                        else  (lookup ((let k = v7 - 1
+                                         in
+                                          if  (k < 0)
+                                          then  0
+                                           else  k
+                                         ) / 2) (if  (even v7)
+                                                      then  v3
+                                                       else  v2)))
+    |   Bs(v6,v5,v4) ->  (if  (v7 <= 0)
+                          then  (Some(v5))
+                          else  (lookup ((let k = v7 - 1
+                                           in
+                                            if  (k < 0)
+                                            then  0
+                                             else  k
+                                           ) / 2) (if  (even v7)
+                                                        then  v6
+                                                         else  v4)))
+
+  let  rec insert v7 v8 v9 =
+    match  v9
+    with  Ln ->  (if  (v7 <= 0)
+                then  (Ls(v8))
+                else  (if  (even v7)
+                       then  (Bn(insert ((let   k = v7 - 1
+                                           in
+                                            if  (k < 0)
+                                            then  0
+                                             else  k
+                                           ) / 2) v8 Ln,Ln))
+                       else  (Bn(Ln,insert ((let   k = v7 - 1
+                                              in
+                                               if  (k < 0)
+                                               then  0
+                                                else  k
+                                              ) / 2) v8 Ln))))
+    |   Ls(v1) ->  (if  (v7 <= 0)
+                    then  (Ls(v8))
+                    else  (if  (even v7)
+                           then  (Bs(insert ((let   k = v7 - 1
+                                               in
+                                                if  (k < 0)
+                                                then  0
+                                                 else  k
+                                               ) / 2) v8 Ln,v1,Ln))
+                           else  (Bs(Ln,v1,insert ((let   k = v7 - 1
+                                                     in
+                                                      if  (k < 0)
+                                                      then  0
+                                                       else  k
+                                                     ) / 2) v8 Ln))))
+    |   Bn(v3,v2) ->  (if  (v7 <= 0)
+                       then  (Bs(v3,v8,v2))
+                       else  (if  (even v7)
+                              then  (Bn(insert ((let   k = v7 - 1
+                                                  in
+                                                   if  (k < 0)
+                                                   then  0
+                                                    else  k
+                                                  ) / 2) v8 v3,v2))
+                              else  (Bn(v3,insert ((let   k = v7 - 1
+                                                     in
+                                                      if  (k < 0)
+                                                      then  0
+                                                       else  k
+                                                     ) / 2) v8 v2))))
+    |   Bs(v6,v5,v4) ->  (if  (v7 <= 0)
+                          then  (Bs(v6,v8,v4))
+                          else  (if  (even v7)
+                                 then  (Bs(insert ((let   k = v7 - 1
+                                                     in
+                                                      if  (k < 0)
+                                                      then  0
+                                                       else  k
+                                                     ) / 2) v8 v6,v5,v4))
+                                 else  (Bs(v6,v5,insert ((let   k =
+                                                            v7 - 1
+                                                           in
+                                                            if  (k < 0)
+                                                            then  0
+                                                             else  k
+                                                           ) / 2) v8 v4))))
+
+
+let rec insert1 a n l =
+  if n < l then
+    (a := insert n 1 !a;
+     insert1 a (n + 6561) l)
+  else if n > l then
+    insert1 a (n - l) l
+  else
+    ()
+
+let rec lookup1 a n l =
+  if n < l then
+    (ignore (lookup n (!a));
+     lookup1 a (n + 6561) l)
+  else if n > l then
+    lookup1 a (n - l) l
+  else
+    ()
+
+let rec ins_look a n len =
+  if n = 0 then
+    ()
+  else
+    (insert1 a 0 len; lookup1 a 0 len; ins_look a (n - 1) len)
+
+let harness n =
+let a = ref Ln in
+  ins_look a 10000 n
+
+let test = harness 40000
