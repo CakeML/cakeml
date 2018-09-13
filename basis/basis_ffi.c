3ab24c37a12ee8f6edcee23fa6f554a3810b4e9a
Michael Sproul <michael.sproul@data61.csiro.au>
2018-09-13 10:32:54 +1000

    FFI: create files with sensible (0644) permissions
    When creating a file on Unix using `open` and `O_CREAT`, we must also provide
    permissions flags to prevent the file being created with garbage permissions.
    We follow PolyML and default to 0644 (readable and writeable by the user,
    readable by group and others).

--- basis/basis_ffi.c
+++ basis/basis_ffi.c
@@ -77,7 +77,7 @@ void ffiopen_in (unsigned char *c, long clen, unsigned char *a, long alen) {
 }
 
 void ffiopen_out (unsigned char *c, long clen, unsigned char *a, long alen) {
-  int fd = open((const char *) c, O_RDWR|O_CREAT|O_TRUNC);
+  int fd = open((const char *) c, O_RDWR|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
   if (0 <= fd){
     a[0] = 0;
     int_to_byte8(fd, &a[1]);
