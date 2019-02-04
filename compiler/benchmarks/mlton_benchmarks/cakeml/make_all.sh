0734e8bc89c518c5bc6ed45139d1c3da2ae8ecd2
Yong Kiam <tanyongkiam@gmail.com>
2019-02-04 11:09:58 -0500

    Use default Makefile in mlton_benchmarks/cml

--- compiler/benchmarks/mlton_benchmarks/cakeml/make_all.sh
+++ compiler/benchmarks/mlton_benchmarks/cakeml/make_all.sh
@@ -1,47 +1,47 @@
-#Build the compiler
-make compiler
+#Cleanup
+make clean
 
 #No clos optimizations
 mkdir -p noclos
-make CAKE_PREFIX=cake_ PATH_PREFIX=./noclos CAKE_FLAGS="--multi=false --known=false --call=false --max_app=1"
+make CAKEFLAGS="--multi=false --known=false --call=false --max_app=1"
+mv *.cake noclos/
 
-##No BVL optimizations
+#No BVL optimizations
 mkdir -p nobvl
-make CAKE_PREFIX=cake_ PATH_PREFIX=./nobvl CAKE_FLAGS="--inline_size=0 --exp_cut=10000 --split=false"
+make CAKEFLAGS="--inline_size=0 --exp_cut=10000 --split=false"
+mv *.cake nobvl/
 
 #No register allocator
 mkdir -p noalloc
-make CAKE_PREFIX=cake_ PATH_PREFIX=./noalloc CAKE_FLAGS="--reg_alg=0"
+make CAKEFLAGS="--reg_alg=0"
+mv *.cake noalloc/
 
-#All optimizations enabled
+#All default optimizations enabled
 mkdir -p all
-make CAKE_PREFIX=cake_ PATH_PREFIX=./all
+make
+mv *.cake all/
 
 #GC debug enabled
-#mkdir -p gc
-#make CAKE_PREFIX=cake_ PATH_PREFIX=./gc CAKE_FLAGS="--emit_empty_ffi=true" FLAGS='-g -D"DEBUG_FFI" -o'
-#
-##Smaller heap size for GC
-#mkdir -p gc2
-#make CAKE_PREFIX=cake_ PATH_PREFIX=./gc2 CAKE_FLAGS="--emit_empty_ffi=true --heap_size=100" FLAGS='-g -D"DEBUG_FFI" -o'
-#
-#mkdir -p gc3
-#make CAKE_PREFIX=cake_ PATH_PREFIX=./gc3 CAKE_FLAGS="--emit_empty_ffi=true --heap_size=10" FLAGS='-g -D"DEBUG_FFI" -o'
+mkdir -p gc
+make CAKEFLAGS="--emit_empty_ffi=true" FLAGS='-g -D"DEBUG_FFI" -o'
+make
+mv *.cake gc/
 
+#TODO: this needs to be updated:
 #Compilation to different targets
 #mkdir -p arm8
 #SKIPGCC=T make CAKE_PREFIX=cake_ PATH_PREFIX=./arm8 CAKE_FLAGS="--target=arm8"
-#
+
 #mkdir -p riscv
 #SKIPGCC=T make CAKE_PREFIX=cake_ PATH_PREFIX=./riscv CAKE_FLAGS="--target=riscv"
-#
+
 #mkdir -p mips
 #SKIPGCC=T make CAKE_PREFIX=cake_ PATH_PREFIX=./mips CAKE_FLAGS="--target=mips --no_jump"
-#
-##mkdir -p x64
-##SKIPGCC=T make CAKE_PREFIX=cake_ PATH_PREFIX=./x64 CAKE_FLAGS="--target=x64"
-#
-##For arm6, we need the 32-bit compiler
+
+#mkdir -p x64
+#SKIPGCC=T make CAKE_PREFIX=cake_ PATH_PREFIX=./x64 CAKE_FLAGS="--target=x64"
+
+#For arm6, we need the 32-bit compiler
 #make compiler32
 #mkdir -p arm6
 #CAKECC=cake32 SKIPGCC=T make CAKE_PREFIX=cake_ PATH_PREFIX=./arm6 CAKE_FLAGS="--target=arm6 --heap_size=500 --stack_size=500"
