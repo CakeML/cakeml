CakeML/cakeml

commit a7a77056a7bd2b7dfb22ce550461f6a6f62c11a3

Fix parsing of tuples (make non-assoc)

candle/parser/camlPtreeConversionScript.sml
@@ -701,14 +701,11 @@ Definition ptree_Pattern_def:
       | _ => fail (locs, «Impossible: nPCons»)
     else if nterm = INL nPProd then
       case args of
-        [pat] => ptree_Pattern pat
-      | [p1; comma; p2] =>
+        pat::pats =>
           do
-            expect_tok comma CommaT;
-            ps <- ptree_Pattern p1;
-            qs <- ptree_Pattern p2;
-            return (MAP (λ(p,q). Pcon NONE [p; q])
-                        (cart_prod ps qs))
+            p <- ptree_Pattern pat;
+            ps <- ptree_PatternCommas pats;
+            return (MAP (λps. Pcon NONE ps) (list_cart_prod (p::ps)))
           od
       | _ => fail (locs, «Impossible: nPProd»)
     else if nterm = INL nPOr then
@@ -749,9 +746,22 @@ Definition ptree_Pattern_def:
        q <- ptree_Pattern p;
        qs <- ptree_PatternList ps;
        return (q::qs)
-     od)
+     od) ∧
+  (ptree_PatternCommas [] = return []) ∧
+  (ptree_PatternCommas (p::ps) =
+    do
+      expect_tok p CommaT;
+      ptree_PatternCommas ps
+    od ++
+    do
+      q <- ptree_Pattern p;
+      qs <- ptree_PatternCommas ps;
+      return (q::qs)
+    od)
 Termination
-  WF_REL_TAC ‘measure (sum_size psize (list_size psize))’
+  WF_REL_TAC ‘measure $ sum_size psize
+                      $ sum_size (list_size psize)
+                                 (list_size psize)’
   \\ simp [parsetree_size_lemma]
 End
 
@@ -1175,13 +1185,11 @@ Definition ptree_Expr_def:
       | _ => fail (locs, «Impossible: nEOr»)
     else if nterm = INL nEProd then
       case args of
-        [exp] => ptree_Expr nEOr exp
-      | [lhs; comma; rhs] =>
+        exp::exps =>
           do
-            expect_tok comma CommaT;
-            x <- ptree_Expr nEOr lhs;
-            y <- ptree_Expr nEProd rhs;
-            return (Con NONE [x; y])
+            x <- ptree_Expr nEOr exp;
+            xs <- ptree_ExprCommas exps;
+            return (Con NONE (x::xs))
           od
       | _ => fail (locs, «Impossible: nEProd»)
     else if nterm = INL nEIf then
@@ -1513,7 +1521,18 @@ Definition ptree_Expr_def:
        y <- ptree_Expr nExpr x;
        ys <- ptree_ExprList xs;
        return (y::ys)
-     od)
+     od) ∧
+  (ptree_ExprCommas [] = return []) ∧
+  (ptree_ExprCommas (x::xs) =
+    do
+      expect_tok x CommaT;
+      ptree_ExprCommas xs
+    od ++
+    do
+      y <- ptree_Expr nEOr x;
+      ys <- ptree_ExprCommas xs;
+      return (y::ys)
+    od)
 Termination
   WF_REL_TAC ‘measure $ sum_size (pair_size camlNT_size psize)
                       $ sum_size psize
@@ -1521,7 +1540,8 @@ Termination
                       $ sum_size psize
                       $ sum_size psize
                       $ sum_size psize
-                      $ sum_size psize (list_size psize)’
+                      $ sum_size psize
+                      $ sum_size (list_size psize) (list_size psize)’
   \\ simp [parsetree_size_lemma]
 End
 

candle/parser/camlPEGScript.sml
@@ -609,7 +609,7 @@ Definition camlPEG_def[nocompute]:
             (bindNT nEOr));
       (* -- Expr4 ---------------------------------------------------------- *)
       (INL nEProd,
-       seql [pnt nEOr; try (seql [tokeq CommaT; pnt nEProd] I)]
+       seql [pnt nEOr; try (rpt (seql [tokeq CommaT; pnt nEOr] I) FLAT)]
             (bindNT nEProd));
       (* -- Expr3: assignments --------------------------------------------- *)
       (* -- Expr2 ---------------------------------------------------------- *)
@@ -732,7 +732,7 @@ Definition camlPEG_def[nocompute]:
             (bindNT nPCons));
       (* -- Pat4 ----------------------------------------------------------- *)
       (INL nPProd,
-       seql [pnt nPCons; try (seql [tokeq CommaT; pnt nPProd] I)]
+       seql [pnt nPCons; try (rpt (seql [tokeq CommaT; pnt nPCons] I) FLAT)]
             (bindNT nPProd));
       (* -- Pat3 ----------------------------------------------------------- *)
       (INL nPOr,

candle/parser/caml_ptreeProgScript.sml
@@ -144,7 +144,8 @@ Theorem ptree_Expr_preconds[local]:
   (∀x. ptree_letbindings_side x) ∧
   (∀x. ptree_patternmatches_side x) ∧
   (∀x. ptree_patternmatch_side x) ∧
-  (∀x. ptree_exprlist_side x)
+  (∀x. ptree_exprlist_side x) ∧
+  (∀x. ptree_exprcommas_side x)
 Proof
   ho_match_mp_tac ptree_Expr_ind
   \\ strip_tac

commit bfb8f21bf061bc7895e6368297465ab189bbab60

Fix bug introduced in a7a7705
Don't tuple up singleton 'tulples'

candle/parser/camlPtreeConversionScript.sml
@@ -701,7 +701,8 @@ Definition ptree_Pattern_def:
       | _ => fail (locs, «Impossible: nPCons»)
     else if nterm = INL nPProd then
       case args of
-        pat::pats =>
+        [pat] => ptree_Pattern pat
+      | pat::pats =>
           do
             p <- ptree_Pattern pat;
             ps <- ptree_PatternCommas pats;
@@ -1185,7 +1186,8 @@ Definition ptree_Expr_def:
       | _ => fail (locs, «Impossible: nEOr»)
     else if nterm = INL nEProd then
       case args of
-        exp::exps =>
+        [exp] => ptree_Expr nEOr exp
+      | exp::exps =>
           do
             x <- ptree_Expr nEOr exp;
             xs <- ptree_ExprCommas exps;