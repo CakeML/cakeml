a5aca21e9a4e9c88ee62b9acef227be22207aa82
dnezam <55559979+dnezam@users.noreply.github.com>
2024-08-14 14:59:49 +0200

    Improve test summary
    * Break down whether Dafny or CakeML failed
    * Print output together with failed test

--- compiler/dafny/tests/dafny_compiler.py
+++ compiler/dafny/tests/dafny_compiler.py
@@ -24,9 +24,9 @@ def main():
     else:
         print("Running test suite...")
 
-        failures = 0
+        dafny_fails = []
+        cakeml_fails = []
         total = 0
-        failed_tests = []
 
         test_files = sorted(args.test_path.glob("*.dfy"))
         for test_file in test_files:
@@ -40,32 +40,47 @@ def main():
             dfy_result = "\n".join(dfy_result)
 
             print("- Translating the program to CakeML and running it...")
-            cml_result = dafny_compiler(test_file, args.dafny_path,
-                                        args.dafny_to_cakeml_path,
-                                        args.cakeml_path, args.output_path,
-                                        capture_output=True,
-                                        verbose=args.verbose)
+            dafny_failure, result = dafny_compiler(test_file,
+                                                   args.dafny_path,
+                                                   args.dafny_to_cakeml_path,
+                                                   args.cakeml_path,
+                                                   args.output_path,
+                                                   capture_output=True,
+                                                   verbose=args.verbose)
 
-            if dfy_result == cml_result:
+            if dfy_result == result:
                 print(f"\033[92mPASS\033[0m with matching output:")
                 print(dfy_result)
+            elif dafny_failure:
+                print(f"\033[91mDAFNY FAILURE\033[0m")
+                result = result.split("\n")[2:]
+                result = "\n".join(result)
+                print(result)
+                dafny_fails.append((test_file, result))
             else:
                 print(f"\033[91mFAIL\033[0m")
                 print("When ran by Dafny:")
                 print(dfy_result)
                 print("When trying to compile to CakeML and run:")
-                print(cml_result)
-                failures += 1
-                failed_tests.append(test_file)
+                print(result)
+                cakeml_fails.append((test_file, result))
 
             print()
             total += 1
 
         print()
-        print(f"{total - failures} out of {total} tests passed.")
-        if failures != 0:
-            print(f"The following {failures} test(s) failed:")
-            pprint(failed_tests)
+        nr_dafny_fails = len(dafny_fails)
+        nr_cakeml_fails = len(cakeml_fails)
+        total_fails = nr_dafny_fails + nr_cakeml_fails
+        print(f"{total - total_fails} out of {total} tests passed.")
+        print(f"{nr_dafny_fails} Dafny fails, {nr_cakeml_fails} CakeML fails")
+
+        if nr_dafny_fails != 0:
+            print(f"The following test(s) failed because of Dafny:")
+            pprint(dafny_fails)
+        if nr_cakeml_fails != 0:
+            print(f"The following test(s) failed because of CakeML:")
+            pprint(cakeml_fails)
 
 def dafny_compiler(program_path, dafny_path, dafny_to_cakeml_path, cakeml_path,
                    output_path, capture_output, verbose):
@@ -95,7 +110,10 @@ def dafny_compiler(program_path, dafny_path, dafny_to_cakeml_path, cakeml_path,
                 print(f"- Command failed with return code {result.returncode}. "
                       "Retrying...")
         elif failed_attempts >= 3:
-            return result.stdout
+            dafny_failure = True
+            return dafny_failure, result.stdout
+
+    dafny_failure = False
 
     if verbose:
         print("- Compile from Dafny S-expression to CakeML S-expression...")
@@ -134,10 +152,10 @@ def dafny_compiler(program_path, dafny_path, dafny_to_cakeml_path, cakeml_path,
 
     if capture_output:
         result = subprocess.run(command, check=True, capture_output=True, text=True)
-        return result.stdout
+        return dafny_failure, result.stdout
     else:
         subprocess.run(command, check=True)
-        return None
+        return dafny_failure, None
 
 def parse_args():
     """Parse command line arguments."""
