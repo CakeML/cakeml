00ff2d3addf0f7466638b7adb26b7c4ff56b1812
pascal <py@lasnier.com>
2025-07-16 15:18:44 +0100

    list length example

--- compiler/scheme/examples/list.scm
+++ compiler/scheme/examples/list.scm
@@ -1,1 +1,5 @@
-((lambda (y . x) (pair? (cdr x))) 1 2 3)
+(letrec
+    ((length (lambda (x)
+                 (if (null? x) 0
+                     (+ 1 (length (cdr x)))))))
+    ((lambda x (length x)) 1 2 3 5 5 5 5))
