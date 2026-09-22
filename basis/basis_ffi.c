20400206be7cbb895ff58fe4d1ff09dca831f7d9
Yong Kiam <tanyongkiam@gmail.com>
2026-09-22 23:29:32 +0800

    Clean up basis FFI organization and runtime initialization
    Group the basis FFIs by library and put host runtime support after them,
    so unused library blocks and their headers are easier to remove.
    
    - Group headers at the top, keep TextIO encoding helpers with their
      FFIs, and make the encoding helpers private to this file.
    - Remove the duplicate EVAL branch in file opening and guard private
      tracing state and its header with DEBUG_FFI, independently of EVAL.
    - Extract memory allocation and evaluation setup from main, and route
      startup failures through one formatted diagnostic helper with exit 3.
    - Validate positive decimal heap/stack sizes, reject parsing and size
      arithmetic overflow, and use byte pointers for memory layout. Preserve
      the default sizes and contiguous allocation; report invalid values.
    - Use checked sigaction registration with SA_RESTART and a handler that
      only sets a private signal flag. Request POSIX declarations explicitly
      so strict C builds expose the required interfaces.
    - Clarify section comments and parenthesize the Double parsing condition
      without changing its behavior.
    
    Preserve FFI behavior and the existing RWX startup and code installation
    sequence.

--- basis/basis_ffi.c
+++ basis/basis_ffi.c
@@ -2,20 +2,35 @@
   Implements the foreign function interface (FFI) used in the CakeML basis
   library, as a thin wrapper around the relevant system calls.
 */
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200809L
+#endif
+
 #include <assert.h>
 #include <errno.h>
-#include <fcntl.h>
-#include <math.h>
+#include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/* TextIO FFI (POSIX) */
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <unistd.h>
+
+/* Double FFI */
+#include <math.h>
+
+/* GC and tracing */
+#ifdef DEBUG_FFI
+#include <sys/time.h> /* POSIX */
+#endif
+
+/* Interactive evaluation */
 #ifdef EVAL
 #include <signal.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/time.h>
+#include <sys/mman.h> /* POSIX */
 #endif
 
 /* This flag is on by default. It catches CakeML's out-of-memory exit codes
@@ -24,57 +39,30 @@
  * */
 #define STDERR_MEM_EXHAUST
 
-/* clFFI (command line) */
-
+/* Host runtime arguments, shared with the CommandLine FFI. */
 unsigned int argc;
 char **argv;
 
-/* exported in cake.S */
+/* exported in CakeML .S file */
 extern void cml_main(void);
 extern void *cml_heap;
 extern void *cml_stack;
 extern void *cml_stackend;
 
-extern char cake_text_begin;
-extern char cake_codebuffer_begin;
-extern char cake_codebuffer_end;
-
-#ifdef EVAL
-
-/* Signal handler for SIGINT */
-
-/* This is set to 1 when the runtime traps a SIGINT */
-volatile sig_atomic_t caught_sigint = 0;
-
-void do_sigint(int sig_num)
-{
-    signal(SIGINT, do_sigint);
-    caught_sigint = 1;
-}
-
-void ffipoll_sigint (unsigned char *c, long clen, unsigned char *a, long alen)
-{
-    if (alen < 1) {
-        return;
-    }
-    a[0] = (unsigned char) caught_sigint;
-    caught_sigint = 0;
-}
-
-void ffikernel_ffi (unsigned char *c, long clen, unsigned char *a, long alen) {
-    for (long i = 0; i < clen; i++) {
-        putc(c[i], stdout);
-    }
-}
-
-#else
-
-void ffipoll_sigint (unsigned char *c, long clen, unsigned char *a, long alen) { }
-
-void ffikernel_ffi (unsigned char *c, long clen, unsigned char *a, long alen) { }
-
+static void cml_runtime_error(int error, const char *format, ...) {
+#ifdef STDERR_MEM_EXHAUST
+  va_list args;
+  va_start(args, format);
+  fputs("CakeML: ", stderr);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  if (error) fprintf(stderr, ": %s", strerror(error));
+  fputs(".\n", stderr);
 #endif
+  exit(3);
+}
 
+/* CommandLine FFI (clFFI) */
 void ffiget_arg_count (unsigned char *c, long clen, unsigned char *a, long alen) {
   a[0] = (char) argc;
   a[1] = (char) (argc / 256);
@@ -97,17 +85,18 @@ void ffiget_arg (unsigned char *c, long clen, unsigned char *a, long alen) {
   }
 }
 
-void int_to_byte2(int i, unsigned char *b){
+/* TextIO FFI (fsFFI): file descriptors and byte encodings */
+static void int_to_byte2(int i, unsigned char *b){
     /* i is encoded on 2 bytes */
     b[0] = (i >> 8) & 0xFF;
     b[1] = i & 0xFF;
 }
 
-int byte2_to_int(unsigned char *b){
+static int byte2_to_int(unsigned char *b){
     return ((b[0] << 8) | b[1]);
 }
 
-void int_to_byte8(int i, unsigned char *b){
+static void int_to_byte8(int i, unsigned char *b){
     /* i is encoded on 8 bytes */
     /* i is cast to long long to ensure having 64 bits */
     /* assumes CHAR_BIT = 8. use static assertion checks? */
@@ -121,14 +110,11 @@ void int_to_byte8(int i, unsigned char *b){
     b[7] =  (long long) i & 0xFF;
 }
 
-int byte8_to_int(unsigned char *b){
+static int byte8_to_int(unsigned char *b){
     return (((long long) b[0] << 56) | ((long long) b[1] << 48) |
              ((long long) b[2] << 40) | ((long long) b[3] << 32) |
              (b[4] << 24) | (b[5] << 16) | (b[6] << 8) | b[7]);
 }
-
-
-/* fsFFI (file system and I/O) */
 
 void ffiopen_in (unsigned char *c, long clen, unsigned char *a, long alen) {
   assert(9 <= alen);
@@ -143,11 +129,7 @@ void ffiopen_in (unsigned char *c, long clen, unsigned char *a, long alen) {
 
 void ffiopen_out (unsigned char *c, long clen, unsigned char *a, long alen) {
   assert(9 <= alen);
-  #ifdef EVAL
   int fd = open((const char *) c, O_RDWR|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
-  #else
-  int fd = open((const char *) c, O_RDWR|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
-  #endif
   if (0 <= fd){
     a[0] = 0;
     int_to_byte8(fd, &a[1]);
@@ -195,102 +177,10 @@ void fficlose (unsigned char *c, long clen, unsigned char *a, long alen) {
   else a[0] = 1;
 }
 
-/* GC FFI */
-int inGC = 0;
-struct timeval t1,t2,lastT;
-long microsecs = 0;
-int numGC = 0;
-int hasT = 0;
-long prevOcc = 0;
-long numAllocBytes = 0;
-
-void cml_exit(int arg) {
-
-  #ifdef STDERR_MEM_EXHAUST
-  if (arg != 0) {
-    fprintf(stderr,"Program exited with nonzero exit code.\n");
-  }
-  #endif
-
-  #ifdef DEBUG_FFI
-  {
-    if(arg == 1) {
-      fprintf(stderr,"CakeML heap space exhausted.\n");
-    }
-    else if(arg == 2) {
-      fprintf(stderr,"CakeML stack space exhausted.\n");
-    }
-    fprintf(stderr,"GCNum: %d, GCTime(us): %ld\n",numGC,microsecs);
-    fprintf(stderr,"Total allocated heap data: %ld bytes\n",numAllocBytes);
-  }
-  #endif
-
-  exit(arg);
-}
-
-void cml_err(int arg) {
-  if (arg == 3) {
-    fprintf(stderr,"Memory not ready for entry. You may have not run the init code yet, or be trying to enter during an FFI call.\n");
-  }
-
-  cml_exit(arg);
-}
-
+/* Runtime FFI */
 void ffiexit (unsigned char *c, long clen, unsigned char *a, long alen) {
   assert(alen == 1);
   exit((int)a[0]);
-}
-
-
-/* empty FFI (assumed to do nothing, but can be used for tracing/logging) */
-void ffi (unsigned char *c, long clen, unsigned char *a, long alen) {
-  #ifdef DEBUG_FFI
-  {
-    if (clen == 0)
-    {
-      if(inGC==1)
-      {
-        gettimeofday(&t2, NULL);
-        microsecs += (t2.tv_usec - t1.tv_usec) + (t2.tv_sec - t1.tv_sec)*1e6;
-        numGC++;
-        inGC = 0;
-        long occ = (long)c; // number of bytes in occupied in heap (all live after standard GC)
-        // long len = (long)a;
-        // fprintf(stderr,"GC stops  %ld %ld \n",occ,len);
-        prevOcc = occ;
-      }
-      else
-      {
-        inGC = 1;
-        gettimeofday(&t1, NULL);
-        long occ = (long)c;
-        // long len = (long)a;
-        // fprintf(stderr,"GC starts %ld %ld \n",occ,len);
-        numAllocBytes += (occ - prevOcc);
-      }
-    } else {
-      int indent = 30;
-      for (int i=0; i<clen; i++) {
-        putc(c[i],stderr);
-        indent--;
-      }
-      for (int i=0; i<indent; i++) {
-        putc(' ',stderr);
-      }
-      struct timeval nowT;
-      gettimeofday(&nowT, NULL);
-      if (hasT) {
-        long usecs = (nowT.tv_usec - lastT.tv_usec) +
-                     (nowT.tv_sec - lastT.tv_sec)*1e6;
-        fprintf(stderr," --- %ld milliseconds\n",usecs / (long)1000);
-      } else {
-        fprintf(stderr,"\n");
-      }
-      gettimeofday(&lastT, NULL);
-      hasT = 1;
-    }
-  }
-  #endif
 }
 
 void fficustom (unsigned char *c, long clen, unsigned char *a, long alen) {
@@ -298,10 +188,7 @@ void fficustom (unsigned char *c, long clen, unsigned char *a, long alen) {
   assert(0 <= clen);
 }
 
-// ---------------------------------------------------------------------------
-// Functions on doubles for the Double module
-// ---------------------------------------------------------------------------
-
+/* Double FFI */
 typedef union {
     double num;
     char bytes[sizeof(double)];
@@ -317,7 +204,7 @@ void ffidouble_fromString(char *c, long clen, char *a, long alen) {
     char *endp;
     errno = 0;
     d.num = strtod(c, &endp);
-    if (errno == ERANGE || endp && *endp != '\0') {
+    if (errno == ERANGE || (endp && *endp != '\0')) {
         a[0] = 1;
     } else {
         a[0] = 0;
@@ -376,6 +263,161 @@ void ffidouble_floor(char *c, long clen, char *a, long alen) {
     memcpy(a, d.bytes, sizeof d.bytes);
 }
 
+/* Interactive evaluation: Interrupt and Candle kernel FFIs */
+#ifdef EVAL
+/* exported in CakeML .S file */
+extern char cake_text_begin;
+extern char cake_codebuffer_end;
+
+/* Signal handler for SIGINT */
+/* This is set to 1 when the runtime traps a SIGINT */
+static volatile sig_atomic_t caught_sigint = 0;
+
+static void do_sigint(int sig_num)
+{
+    caught_sigint = 1;
+}
+
+void ffipoll_sigint (unsigned char *c, long clen, unsigned char *a, long alen)
+{
+    if (alen < 1) {
+        return;
+    }
+    a[0] = (unsigned char) caught_sigint;
+    caught_sigint = 0;
+}
+
+void ffikernel_ffi (unsigned char *c, long clen, unsigned char *a, long alen) {
+    for (long i = 0; i < clen; i++) {
+        putc(c[i], stdout);
+    }
+}
+
+static void cml_init_eval(int local_argc, char **local_argv) {
+  if (mprotect(&cake_text_begin, &cake_codebuffer_end - &cake_text_begin,
+               PROT_READ | PROT_WRITE | PROT_EXEC) != 0) {
+    cml_runtime_error(errno, "cannot protect the text and code buffer");
+  }
+  for (int i = 0; i < local_argc; i++) {
+    if (strcmp(local_argv[i], "--repl") == 0 ||
+        strcmp(local_argv[i], "--candle") == 0) {
+      struct sigaction action = {0};
+      action.sa_handler = do_sigint;
+      action.sa_flags = SA_RESTART;
+      sigemptyset(&action.sa_mask);
+      if (sigaction(SIGINT, &action, NULL) != 0) {
+        cml_runtime_error(errno, "cannot install the SIGINT handler");
+      }
+      break;
+    }
+  }
+}
+
+#else
+
+void ffipoll_sigint (unsigned char *c, long clen, unsigned char *a, long alen) { }
+
+void ffikernel_ffi (unsigned char *c, long clen, unsigned char *a, long alen) { }
+
+static void cml_init_eval(int local_argc, char **local_argv) { }
+
+#endif
+
+/* Host runtime: exit handling, GC and tracing FFI */
+#ifdef DEBUG_FFI
+static int inGC = 0;
+static struct timeval t1,t2,lastT;
+static long microsecs = 0;
+static int numGC = 0;
+static int hasT = 0;
+static long prevOcc = 0;
+static long numAllocBytes = 0;
+#endif
+
+void cml_exit(int arg) {
+
+  #ifdef STDERR_MEM_EXHAUST
+  if (arg != 0) {
+    fprintf(stderr,"Program exited with nonzero exit code.\n");
+  }
+  #endif
+
+  #ifdef DEBUG_FFI
+  {
+    if(arg == 1) {
+      fprintf(stderr,"CakeML heap space exhausted.\n");
+    }
+    else if(arg == 2) {
+      fprintf(stderr,"CakeML stack space exhausted.\n");
+    }
+    fprintf(stderr,"GCNum: %d, GCTime(us): %ld\n",numGC,microsecs);
+    fprintf(stderr,"Total allocated heap data: %ld bytes\n",numAllocBytes);
+  }
+  #endif
+
+  exit(arg);
+}
+
+void cml_err(int arg) {
+  if (arg == 3) {
+    fprintf(stderr,"Memory not ready for entry. You may have not run the init code yet, or be trying to enter during an FFI call.\n");
+  }
+
+  cml_exit(arg);
+}
+
+/* empty FFI (assumed to do nothing, but can be used for tracing/logging) */
+void ffi (unsigned char *c, long clen, unsigned char *a, long alen) {
+  #ifdef DEBUG_FFI
+  {
+    if (clen == 0)
+    {
+      if(inGC==1)
+      {
+        gettimeofday(&t2, NULL);
+        microsecs += (t2.tv_usec - t1.tv_usec) + (t2.tv_sec - t1.tv_sec)*1e6;
+        numGC++;
+        inGC = 0;
+        long occ = (long)c; // number of bytes in occupied in heap (all live after standard GC)
+        // long len = (long)a;
+        // fprintf(stderr,"GC stops  %ld %ld \n",occ,len);
+        prevOcc = occ;
+      }
+      else
+      {
+        inGC = 1;
+        gettimeofday(&t1, NULL);
+        long occ = (long)c;
+        // long len = (long)a;
+        // fprintf(stderr,"GC starts %ld %ld \n",occ,len);
+        numAllocBytes += (occ - prevOcc);
+      }
+    } else {
+      int indent = 30;
+      for (int i=0; i<clen; i++) {
+        putc(c[i],stderr);
+        indent--;
+      }
+      for (int i=0; i<indent; i++) {
+        putc(' ',stderr);
+      }
+      struct timeval nowT;
+      gettimeofday(&nowT, NULL);
+      if (hasT) {
+        long usecs = (nowT.tv_usec - lastT.tv_usec) +
+                     (nowT.tv_sec - lastT.tv_sec)*1e6;
+        fprintf(stderr," --- %ld milliseconds\n",usecs / (long)1000);
+      } else {
+        fprintf(stderr,"\n");
+      }
+      gettimeofday(&lastT, NULL);
+      hasT = 1;
+    }
+  }
+  #endif
+}
+
+/* Host runtime: code installation */
 /* Called out to by the generated code to install len bytes of freshly
  * compiled code at dest. The contract is install_interfer_ok in
  * compiler/backend/semantics/targetSemScript.sml: the destination must end up
@@ -388,47 +430,28 @@ void *cml_install(uint8_t *src, size_t len, uint8_t *dest) {
   return dest;
 }
 
-int main (int local_argc, char **local_argv) {
-
-  argc = local_argc;
-  argv = local_argv;
-
-  char *heap_env = getenv("CML_HEAP_SIZE");
-  char *stack_env = getenv("CML_STACK_SIZE");
-  char *temp; //used to store remainder of strtoul parse
-
-  unsigned long sz = 1024*1024; // 1 MB unit
-  unsigned long cml_heap_sz = 1024 * sz;    // Default: 1 GB heap
-  unsigned long cml_stack_sz = 1024 * sz;   // Default: 1 GB stack
-
-  // Read CML_HEAP_SIZE env variable (if present)
-  // Warning: strtoul may overflow!
-  if(heap_env != NULL)
-  {
-    cml_heap_sz = strtoul(heap_env, &temp, 10);
-    cml_heap_sz *= sz; //heap size is read in units of MBs
+/* Host runtime: memory allocation and entry point */
+/* Heap and stack sizes are positive decimal numbers of mebibytes. */
+static size_t cml_memory_size(const char *name) {
+  const size_t mib = 1024 * 1024;
+  const char *value = getenv(name);
+  char *end;
+  if (value == NULL) return 1024 * mib;
+  errno = 0;
+  unsigned long long count = strtoull(value, &end, 10);
+  if (*value < '0' || *value > '9' || *end != '\0' || errno == ERANGE ||
+      count == 0 || count > SIZE_MAX / mib) {
+    cml_runtime_error(0, "invalid %s=\"%s\"; expected a positive decimal "
+                      "size in MiB within the addressable range", name, value);
   }
+  return (size_t)count * mib;
+}
 
-  if(stack_env != NULL)
-  {
-    cml_stack_sz = strtoul(stack_env, &temp, 10);
-    cml_stack_sz *= sz; //stack size is read in units of MBs
-  }
-
-  if(cml_heap_sz < sz || cml_stack_sz < sz) //At least 1MB heap and stack size
-  {
-    #ifdef STDERR_MEM_EXHAUST
-    fprintf(stderr,"Too small requested heap (%lu) or stack (%lu) size in bytes.\n",cml_heap_sz, cml_stack_sz);
-    #endif
-    exit(3);
-  }
-
-  if(cml_heap_sz + cml_stack_sz < 8192) // Global minimum heap/stack for CakeML. 4096 for 32-bit architectures
-  {
-    #ifdef STDERR_MEM_EXHAUST
-    fprintf(stderr,"Too small requested heap (%lu) + stack (%lu) size in bytes.\n",cml_heap_sz, cml_stack_sz);
-    #endif
-    exit(3);
+static void cml_init_memory(void) {
+  size_t heap_size = cml_memory_size("CML_HEAP_SIZE");
+  size_t stack_size = cml_memory_size("CML_STACK_SIZE");
+  if (heap_size > SIZE_MAX - stack_size) {
+    cml_runtime_error(0, "heap and stack size overflow");
   }
 
   /**
@@ -448,46 +471,19 @@ int main (int local_argc, char **local_argv) {
    *  The position cml_stack may be (slightly) dynamically adjusted by CakeML,
    *  see `get_stack_heap_limit` in stack_removeProof
    **/
-
-  cml_heap = malloc(cml_heap_sz + cml_stack_sz); // allocate both heap and stack at once
-
-  if(cml_heap == NULL)
-  {
-    #ifdef STDERR_MEM_EXHAUST
-    fprintf(stderr,"failed to allocate sufficient CakeML heap and stack space.\n");
-    perror("malloc");
-    #endif
-    exit(3);
+  cml_heap = malloc(heap_size + stack_size);
+  if (cml_heap == NULL) {
+    cml_runtime_error(errno, "cannot allocate the heap and stack");
   }
+  cml_stack = (uint8_t *)cml_heap + heap_size;
+  cml_stackend = (uint8_t *)cml_stack + stack_size;
+}
 
-  cml_stack = cml_heap + cml_heap_sz;
-  cml_stackend = cml_stack + cml_stack_sz;
-
-  #ifdef EVAL
-
-  /** Set up the "eval" code buffer to be read-write-execute. **/
-  if(mprotect(&cake_text_begin, &cake_codebuffer_end - &cake_text_begin,
-              PROT_READ | PROT_WRITE | PROT_EXEC))
-  {
-    #ifdef STDERR_MEM_EXHAUST
-    fprintf(stderr,"failed to set permissions for CakeML code buffer.\n");
-    perror("mprotect");
-    #endif
-    exit(3);
-  }
-
-  /* Set up the signal handler for SIGINTs when running the REPL. */
-  for (int i = 0; i < local_argc; i++) {
-      if (strcmp(local_argv[i], "--repl") == 0 ||
-          strcmp(local_argv[i], "--candle") == 0) {
-        signal(SIGINT, do_sigint);
-        break;
-      }
-  }
-
-  #endif
-
-  cml_main(); // Passing control to CakeML
-
+int main(int local_argc, char **local_argv) {
+  argc = local_argc;
+  argv = local_argv;
+  cml_init_memory();
+  cml_init_eval(local_argc, local_argv);
+  cml_main();
   return 0;
 }
